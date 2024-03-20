c292f5ff5df4b71e61f78ed8f768c1366f7c89a7
dr-scooby <hinurali@hotmail.com>
2024-03-19 21:29:09 -0700

    push to SpringProduct github

---
+++ SpringProductMongoDB/src/main/java/com/jah/springproduct/SpringProductMongoDbApplication.java
@@ -0,0 +1,14 @@
+package com.jah.springproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProductMongoDbApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello spring");
+		SpringApplication.run(SpringProductMongoDbApplication.class, args);
+	}
+
+}
---
+++ SpringProductMongoDB/src/test/java/com/jah/springproduct/SpringProductMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jah.springproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProductMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
