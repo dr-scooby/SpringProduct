a550cd3d0acf4893da87502cf991dfe9e7a12556
dr-scooby <hinurali@hotmail.com>
2024-03-21 15:19:59 -0700

    search working using key value

--- SpringProductMongoDB/src/main/java/com/jah/springproduct/PostController.java
+++ SpringProductMongoDB/src/main/java/com/jah/springproduct/PostController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +21,18 @@ import springfox.documentation.annotations.ApiIgnore;
 
 import com.jah.springproduct.model.Post;
 import com.jah.springproduct.model.PostRepository;
+import com.jah.springproduct.model.SearchRepository;
 
 @RestController
 public class PostController {
 	
 	@Autowired
 	PostRepository repo; // Spring will create the class of the PostRepository
+	
+	@Autowired
+	SearchRepository srepo; // our own custom search
+	
+	
 	
 	// ApiIgnore will remove swagger operations, we can build our own
 	// remove ApiIgnore to see swagger operations
@@ -46,5 +53,13 @@ public class PostController {
     @PostMapping("/addPost")
     public Post addPost(@RequestBody Post post) {
     	return repo.save(post);
+    }
+    
+    
+    // search, get client data to search using {text}
+    @GetMapping("/posts/{text}")
+    public List<Post> search(@PathVariable String text){
+    	
+    	return srepo.findByText(text);
     }
 }
